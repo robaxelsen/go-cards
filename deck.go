308e73242b396854a251c03719608694168311ed
Robert Axelsen <rob@rob.ee>
2017-10-01 21:44:50 +0200

    Add saveToFile() method

--- deck.go
+++ deck.go
@@ -3,6 +3,7 @@ package main
 import (
     "fmt"
     "strings"
+    "io/ioutil"
 )
 
 // create a new type of 'deck'
@@ -37,3 +38,8 @@ func deal(d deck, handSize int) (deck, deck) {
 func (d deck) toString() string {
     return strings.Join([]string(d), ",")
 }
+
+func (d deck) saveToFile(filename string) error {
+    return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
