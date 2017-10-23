5e48bb4d4a862d75408d564dde7b798d98a2e6d1
Robert Axelsen <rob@rob.ee>
2017-10-23 21:49:38 +0200

    Add some more struct examples

--- structs.go
+++ structs.go
@@ -1,11 +1,19 @@
 // throwaway structs playground file
 package main
 
+import "fmt"
+
 type person struct {
     firstName string
     lastName string
 }
 
 func main() {
-    alex := person{"Alex", "Anderson"}
+    var alex person
+
+    alex.firstName = "Alex"
+    alex.lastName = "Anderson"
+
+    fmt.Println(alex)
+    fmt.Printf("%+v", alex)
 }
